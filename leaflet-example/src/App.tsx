5975690874bbc40062a0fcf23f0760e2534929bd
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-08-20 11:37:53 +0200

    Added code

---
+++ leaflet-example/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import LeafletMap from './components/LeafletMap';
+
+function App() {
+  return (
+    <main>
+      <LeafletMap />
+    </main>
+  );
+}
+
+export default App;
